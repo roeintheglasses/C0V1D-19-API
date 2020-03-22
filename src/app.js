f3c004d7b6243387e6fc9a6ca8660e04b9c3a54b
Hrishi Jangir <hrkjangir@gmail.com>
2020-03-22 13:37:58 +0530

    Added compression middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
+const compression = require('compression')
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(compression())
 app.use(cors())
 app.use('/api', routes);
 app.get('*', (_, res) => res.send('Page Not found'));
